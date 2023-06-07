4c6d692d4542805c625f8d6f8b107ecc37dc2b29
Nils F. Karlsson <nils@tibiahof.com>
2023-06-07 20:37:39 +0200

    boundary date value extraction fix

--- py_matplanering/core/boundary/boundary_date.py
+++ py_matplanering/core/boundary/boundary_date.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from py_matplanering.core.boundary.boundary_base import BoundaryBase
+from py_matplanering.core.boundary.boundary_base import BoundaryBase, BoundaryError
 from py_matplanering.core.schedule.schedule import ScheduleEvent
 from py_matplanering.core.context import BoundaryContext
 from py_matplanering.utilities import time_helper
@@ -11,7 +11,13 @@ class BoundaryDate(BoundaryBase):
     def filter_eligible_dates(self, boundary_context: BoundaryContext) -> List[str]:
         ret_dates = []
         for outer_row in self._boundary['date']:
-            for inner_row in outer_row['values']:
+            if isinstance(outer_row, str) and outer_row == 'values':
+                iter_outer_row = self._boundary['date']['values']
+            elif isinstance(outer_row, dict):
+                iter_outer_row = outer_row['values']
+            else:
+                raise BoundaryError('Unexpected value provided to boundary: %s (value=%s of type=%s). Expected: str=\'values\' or dict' % (self._boundary, outer_row, type(outer_row)))
+            for inner_row in iter_outer_row:
                 cur_year = time_helper.get_year()
                 date_str = "%s-%s-%s" % (cur_year, inner_row['month_number'], inner_row['day_of_month'])
                 # validates date_str
